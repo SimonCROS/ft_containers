f9911acbac56c4d8b52c9201790280c40afa17fe
Simon CROS <simon.dc.cros@gmail.com>
2021-12-27 09:22:24 +0100

    Save

--- map.hpp
+++ map.hpp
@@ -5,4 +5,27 @@
 #ifndef FT_CONTAINERS_MAP_HPP
 #define FT_CONTAINERS_MAP_HPP
 
+namespace ft {
+	template<class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<pair<const Key,T> > >
+	class map {
+    public:
+		typedef Key key_type;
+        typedef T mapped_type;
+        typedef pair<const key_type, mapped_type> value_type;
+        typedef Allocator allocator_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+        typedef typename ft::__normal_iterator<pointer> iterator;
+        typedef typename ft::__normal_iterator<const_pointer> const_iterator;
+        typedef typename ft::reverse_iterator<iterator> reverse_iterator;
+        typedef typename ft::reverse_iterator<const_iterator> const_reverse_iterator;
+        typedef std::ptrdiff_t difference_type;
+        typedef std::size_t size_type;
+    private:
+    public:
+	};
+}
+
 #endif //FT_CONTAINERS_MAP_HPP
--- vector.hpp
+++ vector.hpp
@@ -11,11 +11,11 @@
 
 namespace ft {
 
-    template<class T, class Allocator = std::allocator<T> >
+    template<class T, class Alloc = std::allocator<T> >
     class vector {
     public:
         typedef T value_type;
-        typedef Allocator allocator_type;
+        typedef Alloc allocator_type;
         typedef typename allocator_type::reference reference;
         typedef typename allocator_type::const_reference const_reference;
         typedef typename allocator_type::pointer pointer;
